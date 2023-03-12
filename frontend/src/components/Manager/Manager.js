390e233fc1bc3f18b13f52f8cc107fe2fde00305
AhmedAlSawalmeh <sawalmeh.ahmed@gmail.com>
2023-03-12 20:52:40 +0300

    working on salary frontend

--- frontend/src/components/Manager/Manager.js
+++ frontend/src/components/Manager/Manager.js
@@ -9,9 +9,12 @@ import "./Manager.css"
 const Manager = () => {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
-    const { token, setToken } = useContext(Data)
-    const [clickId, setClickId] = useState("")
     const [employee, setEmployee] = useState("")
+    const { token, setToken } = useContext(Data)
+    const [salaryClicked, setSalaryClicked] = useState(false)
+    const [clickId, setClickId] = useState("")
+    const [valueOfSalary, setValueOfSalary] = useState("")
+    const [salaryClickId, setSalaryClickId] = useState("")
     const loadMoreData = async () => {
         if (loading) {
             return;
@@ -46,11 +49,11 @@ const Manager = () => {
             console.log(err)
         }
     }
-    
+
     const calculateAnnual = async (employeeId) => {
-        console.log(employeeId)
-        const employee_Id={
-            employee_id:employeeId
+
+        const employee_Id = {
+            employee_id: employeeId
         }
         try {
             const response = await axios.post("http://localhost:5000/annual", employee_Id, {
@@ -58,18 +61,36 @@ const Manager = () => {
                     Authorization: `Bearer ${token}`
                 }
             })
-            console.log(response)
+
         } catch (err) {
             console.log(err)
         }
 
     }
     const calculateSick = async (employeeId) => {
-        const employee_Id={
-            employee_id:employeeId
+        const employee_Id = {
+            employee_id: employeeId
         }
         try {
             const response = await axios.post("http://localhost:5000/sick", employee_Id, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+    const calculateSalary = async (employeeId) => {
+        const employee = {
+            employee_id: employeeId,
+            hourly_salary: valueOfSalary
+        }
+        try {
+            const response =await axios.post("http://localhost:5000/salary", employee, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
@@ -79,107 +100,120 @@ const Manager = () => {
         } catch (err) {
             console.log(err)
         }
-
     }
-    if(employee){
+    if (employee) {
         calculateAnnual(employee._id)
         calculateSick(employee._id)
     }
-
-useEffect(() => {
-    loadMoreData();
+    if(salaryClickId){
+        calculateSalary(employee._id)
+    }
     
-       
-    
-   
-}, []);
+    useEffect(() => {
+        loadMoreData();
+        
+        // if(salaryClickId){
+        //     calculateSalary(employee._id)
+        // }
+    }, []);
 
 
-return (
-    <div className='managerPage'>
-        <div
-            id="scrollableDiv"
-            style={{
-                width: 350,
-                height: "100vh",
-                overflow: 'auto',
-                padding: '0 16px',
-                border: '1px solid rgba(140, 140, 140, 0.35)',
-            }}
-        >
-            <InfiniteScroll
-                dataLength={data.length}
-                next={loadMoreData}
-                hasMore={data.length < 3}
-                loader={
-                    <Skeleton
-                        avatar
-                        paragraph={{
-                            rows: 1,
-                        }}
-                        active
-                    />
-                }
-                endMessage={<Divider plain>End ..</Divider>}
-                scrollableTarget="scrollableDiv"
+    return (
+        <div className='managerPage'>
+            <div
+                id="scrollableDiv"
+                style={{
+                    width: 350,
+                    height: "100vh",
+                    overflow: 'auto',
+                    padding: '0 16px',
+                    border: '1px solid rgba(140, 140, 140, 0.35)',
+                }}
             >
-                <List
-                    dataSource={data}
-                    renderItem={(employee) => (
-                        <List.Item key={employee.email}>
-                            <List.Item.Meta
+                <InfiniteScroll
+                    dataLength={data.length}
+                    next={loadMoreData}
+                    hasMore={data.length < 3}
+                    loader={
+                        <Skeleton
+                            avatar
+                            paragraph={{
+                                rows: 1,
+                            }}
+                            active
+                        />
+                    }
+                    endMessage={<Divider plain>End ..</Divider>}
+                    scrollableTarget="scrollableDiv"
+                >
+                    <List
+                        dataSource={data}
+                        renderItem={(employee) => (
+                            <List.Item key={employee.email}>
+                                <List.Item.Meta
 
-                                title={<a onClick={() => {
-                                    handleClickName(employee._id)
+                                    title={<a onClick={() => {
+                                        handleClickName(employee._id)
 
-                                }}>{employee.name}</a>}
+                                    }}>{employee.name}</a>}
 
-                                description={employee.email}
+                                    description={employee.email}
 
-                            />
+                                />
 
-                            <div>{employee.employeeId}</div>
-                        </List.Item>
-                    )}
-                />
-            </InfiniteScroll>
+                                <div>{employee.employeeId}</div>
+                            </List.Item>
+                        )}
+                    />
+                </InfiniteScroll>
 
+            </div>
+            <div>
+                {employee && <div className='emplyoeeProfile'> <Descriptions title={`${employee.name}'s Profile`} layout="vertical" bordered={true}>
+                    <Descriptions.Item label="Name">{employee.name}</Descriptions.Item>
+                    <Descriptions.Item label="Employee ID">{employee.employeeId}</Descriptions.Item>
+                    <Descriptions.Item label="Phone Number">{employee.phoneNumber}</Descriptions.Item>
+                    <Descriptions.Item label="Date of Entry">{employee.date}</Descriptions.Item>
+                    <Descriptions.Item label="Current Time" span={2}>
+                        {Date()}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Status" span={3} >
+
+                        <Badge status="processing" text="Running" />
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Annual Vacations">{employee.annual_vacations.annual_days}</Descriptions.Item>
+                    <Descriptions.Item label="Sick Vacations">{employee.sick_vacations.sick_days}</Descriptions.Item>
+                    <Descriptions.Item label="Salary"><button onClick={() => {
+                        
+                        setSalaryClickId(employee._id)
+                        setSalaryClicked(!salaryClicked)
+                       
+                    }}>Edit Salary</button>
+                        {salaryClicked && salaryClickId === employee._id && <input placeholder="Hourly" onChange={(e) => {
+                            setValueOfSalary(e.target.value)
+                        }}></input>}
+                        <p>{employee.salary.hourly_salary*8}JOD</p>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="HR Actions">
+                        Data disk type: MongoDB
+                        <br />
+                        Database version: 3.4
+                        <br />
+                        Package: dds.mongo.mid
+                        <br />
+                        Storage space: 10 GB
+                        <br />
+                        Replication factor: 3
+                        <br />
+                        Region: East China 1
+                        <br />
+                    </Descriptions.Item>
+                </Descriptions>
+                </div>}
+
+            </div>
         </div>
-        <div>
-            {employee && <div className='emplyoeeProfile'> <Descriptions title={`${employee.name}'s Profile`} layout="vertical" bordered={true}>
-                <Descriptions.Item label="Name">{employee.name}</Descriptions.Item>
-                <Descriptions.Item label="Employee ID">{employee.employeeId}</Descriptions.Item>
-                <Descriptions.Item label="Phone Number">{employee.phoneNumber}</Descriptions.Item>
-                <Descriptions.Item label="Date of Entry">{employee.date}</Descriptions.Item>
-                <Descriptions.Item label="Current Time" span={2}>
-                    {Date()}
-                </Descriptions.Item>
-                <Descriptions.Item label="Status" span={3} >
-
-                    <Badge status="processing" text="Running" />
-                </Descriptions.Item>
-                <Descriptions.Item label="Annual Vacations">{employee.annual_vacations.annual_days}</Descriptions.Item>
-                <Descriptions.Item label="Sick Vacations">{employee.sick_vacations.sick_days}</Descriptions.Item>
-                <Descriptions.Item label="Salary">$60.00</Descriptions.Item>
-                <Descriptions.Item label="HR Actions">
-                    Data disk type: MongoDB
-                    <br />
-                    Database version: 3.4
-                    <br />
-                    Package: dds.mongo.mid
-                    <br />
-                    Storage space: 10 GB
-                    <br />
-                    Replication factor: 3
-                    <br />
-                    Region: East China 1
-                    <br />
-                </Descriptions.Item>
-            </Descriptions>
-            </div>}
-
-        </div>
-    </div>
-) }
+    )
+}
 
 export default Manager
