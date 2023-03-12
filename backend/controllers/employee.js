ead14243632466f061c740b12415e082ae32c942
AhmedAlSawalmeh <sawalmeh.ahmed@gmail.com>
2023-03-12 18:20:23 +0300

    still working the manager page

--- backend/controllers/employee.js
+++ backend/controllers/employee.js
@@ -86,131 +86,133 @@ const getEmployeesByRole = (req, res) => {
             })
         })
 }
-const getEmployeesById = (req,res)=>{
-const id = req.params.id
-employeeModel
-.findById({_id:id})
-.then((result)=>{
-    if(!result){
-        return res.status(404).json({
-            sucsses: false,
-            message: "There is no employee for this id"
-        })
-    }
-    res.status(200).json({
-        sucsses:true,
-        message:"The employee details for this id",
-        employee:result
-    })
-}).catch((err) => {
-    res.status(500).json({
-        sucsses: false,
-        message: "Server Error",
-        err: err.message
-    })
-})
-}
-const deleteEmployeeById =(req,res)=>{
+const getEmployeesById = (req, res) => {
     const id = req.params.id
     employeeModel
-    .findByIdAndDelete({id})
-    .then((result)=>{
-        if(!result){
-            return res.status(404).json({
-                sucsses:false,
-                message:"This employee is not in the company"
+        .findById({ _id: id })
+        .populate("annual_vacations sick_vacations role schedule salary hr_actions")
+        .exec()
+        .then((result) => {
+            if (!result) {
+                return res.status(404).json({
+                    sucsses: false,
+                    message: "There is no employee for this id"
+                })
+            }
+            res.status(200).json({
+                sucsses: true,
+                message: "The employee details for this id",
+                employee: result
             })
-        }
-        res.status(200).json({
-            sucsses:true,
-            message:"Employee's file deleted"
+        }).catch((err) => {
+            res.status(500).json({
+                sucsses: false,
+                message: "Server Error",
+                err: err.message
+            })
         })
-    }).catch((err)=>{
-        res.status(500).json({
-            sucsses:false,
-            message:"Server Error",
-            err:err.message
-        })
-    })
 }
-const updateEmployeeById = (req,res)=>{
+const deleteEmployeeById = (req, res) => {
+    const id = req.params.id
+    employeeModel
+        .findByIdAndDelete({ id })
+        .then((result) => {
+            if (!result) {
+                return res.status(404).json({
+                    sucsses: false,
+                    message: "This employee is not in the company"
+                })
+            }
+            res.status(200).json({
+                sucsses: true,
+                message: "Employee's file deleted"
+            })
+        }).catch((err) => {
+            res.status(500).json({
+                sucsses: false,
+                message: "Server Error",
+                err: err.message
+            })
+        })
+}
+const updateEmployeeById = (req, res) => {
     const id = req.params.id
     const update = req.body
     employeeModel
-    .findByIdAndUpdate({id},update,{new:true})
-    .then((result)=>{
-        if(!result){
-            return res.status(403).json({
-                sucsses:false,
-                message:"This employee is not in the company",
+        .findByIdAndUpdate({ id }, update, { new: true })
+        .then((result) => {
+            if (!result) {
+                return res.status(403).json({
+                    sucsses: false,
+                    message: "This employee is not in the company",
 
+                })
+            }
+            res.status(202).json({
+                sucsses: true,
+                message: "Employee's file updated",
+                employee: result
             })
-        }
-        res.status(202).json({
-            sucsses:true,
-            message:"Employee's file updated",
-            employee:result
-        })
-    }).catch((err)=>{
-        res.status(500).json({
-            sucsses:false,
-            message:"Server Error",
-            err:err.message
-        })
+        }).catch((err) => {
+            res.status(500).json({
+                sucsses: false,
+                message: "Server Error",
+                err: err.message
+            })
 
-    })
+        })
 }
 const login = (req, res) => {
     const password = req.body.password;
     const email = req.body.email.toLowerCase();
     employeeModel
-    .findOne({email})
-    .populate("role")
-    .then(async(result)=>{
-        if(!result){
-            return res.status(403).json({
-                sucsses:false,
-                message:"Check your email or password"
-            })
-        }try{
-            
-            const correct = await bcrypt.compare(password,result.password)
-            if(!correct){
+        .findOne({ email })
+        .populate("role")
+        .then(async (result) => {
+            if (!result) {
                 return res.status(403).json({
-                    sucsses:false,
-                    message:"Check your email or password"
+                    sucsses: false,
+                    message: "Check your email or password"
                 })
-            }
-            const payload={
-                employeeid : result.employeeId,
-                employeeName : result.Name,
-                role:result.role,
-                phoneNumber : result.phoneNumber
-            }
-            const options = {
-                expiresIn:"120m"
-            }
-            const token = jwt.sign(payload,process.env.SECRET,options)
-            res.status(200).json({
-                sucsses:true,
-                message:"Welcome",
-                token:token,
-                role:result.role
-            })
+            } try {
 
-        }catch(err){
-            
-            throw new Error(err.message);
-        }
-        
-    }).catch((err)=>{
-        
-        res.status(500).json({
-            sucsses:false,
-            message:"Server Error",
-            err:err.message
+                const correct = await bcrypt.compare(password, result.password)
+                if (!correct) {
+                    return res.status(403).json({
+                        sucsses: false,
+                        message: "Check your email or password"
+                    })
+                }
+                const payload = {
+                    employeeid: result.employeeId,
+                    employeeName: result.Name,
+                    role: result.role,
+                    phoneNumber: result.phoneNumber
+                }
+                const options = {
+                    expiresIn: "120m"
+                }
+                const token = jwt.sign(payload, process.env.SECRET, options)
+                res.status(200).json({
+                    sucsses: true,
+                    message: "Welcome",
+                    token: token,
+                    role: result.role
+                })
+
+            } catch (err) {
+
+                throw new Error(err.message);
+            }
+
+        }).catch((err) => {
+
+            res.status(500).json({
+                sucsses: false,
+                message: "Server Error",
+                err: err.message
+            })
         })
-    })
 }
 
 
--- frontend/src/components/Manager/Manager.js
+++ frontend/src/components/Manager/Manager.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Manager.css"
-import { Avatar, Divider, List, Skeleton } from 'antd';
+import { Avatar, Divider, List, Skeleton, Descriptions, Badge } from 'antd';
 import { useEffect, useState, useContext } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from 'axios';
@@ -11,7 +11,7 @@ const Manager = () => {
     const [data, setData] = useState([]);
     const { token, setToken } = useContext(Data)
     const [clickId, setClickId] = useState("")
-    const [employee, setEmployee] = useState({})
+    const [employee, setEmployee] = useState("")
     const loadMoreData = async () => {
         if (loading) {
             return;
@@ -40,71 +40,146 @@ const Manager = () => {
                 }
             })
             setEmployee(response.data.employee)
-            console.log(response)
+
 
         } catch (err) {
             console.log(err)
         }
     }
+    
+    const calculateAnnual = async (employeeId) => {
+        console.log(employeeId)
+        const employee_Id={
+            employee_id:employeeId
+        }
+        try {
+            const response = await axios.post("http://localhost:5000/annual", employee_Id, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            console.log(response)
+        } catch (err) {
+            console.log(err)
+        }
 
-    useEffect(() => {
-        loadMoreData();
-    }, []);
-    return (
-        <div className='managerPage'>
-            <div
-                id="scrollableDiv"
-                style={{
-                    width: 350,
-                    height: "100vh",
-                    overflow: 'auto',
-                    padding: '0 16px',
-                    border: '1px solid rgba(140, 140, 140, 0.35)',
-                }}
-            >
-                <InfiniteScroll
-                    dataLength={data.length}
-                    next={loadMoreData}
-                    hasMore={data.length < 3}
-                    loader={
-                        <Skeleton
-                            avatar
-                            paragraph={{
-                                rows: 1,
-                            }}
-                            active
-                        />
-                    }
-                    endMessage={<Divider plain>End ..</Divider>}
-                    scrollableTarget="scrollableDiv"
-                >
-                    <List
-                        dataSource={data}
-                        renderItem={(employee) => (
-                            <List.Item key={employee.email}>
-                                <List.Item.Meta
+    }
+    const calculateSick = async (employeeId) => {
+        const employee_Id={
+            employee_id:employeeId
+        }
+        try {
+            const response = await axios.post("http://localhost:5000/sick", employee_Id, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            console.log(response)
+         
+        } catch (err) {
+            console.log(err)
+        }
 
-                                    title={<a onClick={() => {
-                                        handleClickName(employee._id)
+    }
+    if(employee){
+        calculateAnnual(employee._id)
+        calculateSick(employee._id)
+    }
 
-                                    }}>{employee.name}</a>}
+useEffect(() => {
+    loadMoreData();
+    
+       
+    
+   
+}, []);
 
-                                    description={employee.email}
 
-                                />
-                                {employee._id === clickId && <p>hello</p>}
-                                <div>{employee.phoneNumber}</div>
-                            </List.Item>
-                        )}
+return (
+    <div className='managerPage'>
+        <div
+            id="scrollableDiv"
+            style={{
+                width: 350,
+                height: "100vh",
+                overflow: 'auto',
+                padding: '0 16px',
+                border: '1px solid rgba(140, 140, 140, 0.35)',
+            }}
+        >
+            <InfiniteScroll
+                dataLength={data.length}
+                next={loadMoreData}
+                hasMore={data.length < 3}
+                loader={
+                    <Skeleton
+                        avatar
+                        paragraph={{
+                            rows: 1,
+                        }}
+                        active
                     />
-                </InfiniteScroll>
+                }
+                endMessage={<Divider plain>End ..</Divider>}
+                scrollableTarget="scrollableDiv"
+            >
+                <List
+                    dataSource={data}
+                    renderItem={(employee) => (
+                        <List.Item key={employee.email}>
+                            <List.Item.Meta
 
-            </div>
-            <div>
-                {employee && <p>{employee.name}</p>}
-            </div>
+                                title={<a onClick={() => {
+                                    handleClickName(employee._id)
+
+                                }}>{employee.name}</a>}
+
+                                description={employee.email}
+
+                            />
+
+                            <div>{employee.employeeId}</div>
+                        </List.Item>
+                    )}
+                />
+            </InfiniteScroll>
+
         </div>
-    )
-}
+        <div>
+            {employee && <div className='emplyoeeProfile'> <Descriptions title={`${employee.name}'s Profile`} layout="vertical" bordered={true}>
+                <Descriptions.Item label="Name">{employee.name}</Descriptions.Item>
+                <Descriptions.Item label="Employee ID">{employee.employeeId}</Descriptions.Item>
+                <Descriptions.Item label="Phone Number">{employee.phoneNumber}</Descriptions.Item>
+                <Descriptions.Item label="Date of Entry">{employee.date}</Descriptions.Item>
+                <Descriptions.Item label="Current Time" span={2}>
+                    {Date()}
+                </Descriptions.Item>
+                <Descriptions.Item label="Status" span={3} >
+
+                    <Badge status="processing" text="Running" />
+                </Descriptions.Item>
+                <Descriptions.Item label="Annual Vacations">{employee.annual_vacations.annual_days}</Descriptions.Item>
+                <Descriptions.Item label="Sick Vacations">{employee.sick_vacations.sick_days}</Descriptions.Item>
+                <Descriptions.Item label="Salary">$60.00</Descriptions.Item>
+                <Descriptions.Item label="HR Actions">
+                    Data disk type: MongoDB
+                    <br />
+                    Database version: 3.4
+                    <br />
+                    Package: dds.mongo.mid
+                    <br />
+                    Storage space: 10 GB
+                    <br />
+                    Replication factor: 3
+                    <br />
+                    Region: East China 1
+                    <br />
+                </Descriptions.Item>
+            </Descriptions>
+            </div>}
+
+        </div>
+    </div>
+) }
 
 export default Manager
